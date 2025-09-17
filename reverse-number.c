5e92679d26fccfc373e7c239d0512157f36afc99
DURGESH SINGH <durgesh20336@gmail.com>
2025-09-17 22:46:26 +0530

    Add files via upload

---
+++ reverse-number.c
@@ -0,0 +1,22 @@
+//sum of individual digits 
+#include<stdio.h>
+#include<conio.h>
+
+int main(){
+	
+	int n,rem,rev=0;
+	printf("enter the numbre:\n");
+	scanf("%d",&n);
+	
+	while(n!=0){
+		
+		rem=n%10;
+		rev=(rev*10)+rem;	
+		n=n/10;		
+	}
+	printf("THE REVERSE OF THE NUMEBR =%d\n",rev);
+	
+	getch();
+	return 0;
+	
+}

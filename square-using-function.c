e5168e90ff4cf7cfa7c6c4aa18f16fa373d1491f
DURGESH SINGH <durgesh20336@gmail.com>
2025-09-19 23:45:22 +0530

    Add files via upload

---
+++ square-using-function.c
@@ -0,0 +1,23 @@
+//finding square of a number by using function
+
+#include<stdio.h>
+#include<conio.h>
+void square(int number);
+int main(){
+	
+	int number;
+	
+	printf("\nenter the number:");
+	scanf("%d",&number);
+	
+	square(number);
+	printf("the original value was:%d",number);
+	getch();
+	return 0;
+}
+
+void square(int number){
+	int sq;
+	sq=pow(number,2);
+	printf("squared value:%d\n",sq);
+}

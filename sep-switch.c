e4e77c1d4450e19ce0ce9db9fd8bf87c19f79228
DURGESH SINGH <durgesh20336@gmail.com>
2025-09-23 18:32:03 +0530

    Add files via upload

---
+++ sep-switch.c
@@ -0,0 +1,77 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main(){
+	
+	int date;
+	
+	printf("\nenter the date ?/09/2025:");
+	scanf("%d",&date);
+	
+	switch (date){
+	case 1:printf("The day is MONDAY ON 1/9/25");
+	break;
+	case 2:printf("The day is TUESDAY ON 2/9/25");
+	break;
+	case 3:printf("The day is WEDNESDAY ON 3/9/25");
+	break;
+	case 4:printf("The day is THRUSDAY ON 4/9/25");
+	break;	
+	case 5:printf("The day is FRIDAY ON 5/9/25");
+	break;
+	case 6:printf("The day is SATURDAY ON 6/9/25");
+	break;
+	case 7:printf("The day is SUNDAY ON 7/9/25");
+	break;
+	case 8:printf("The day is MONDAY ON 8/9/25");
+	break;
+	case 9:printf("The day is TUESDAY ON 9/9/25");
+	break;
+	case 10:printf("The day is WEDNESDAY ON 10/9/25");
+	break;
+	case 11:printf("The day is THRUSDAY ON 11/9/25");
+	break;
+	case 12:printf("The day is FRIDAY ON 12/9/25");
+	break;
+	case 13:printf("The day is SATURDAY ON 13/9/25");
+	break;
+	case 14:printf("The day is SUNDAY ON 14/9/25");
+	break;
+	case 15:printf("The day is MONDAY ON 15/9/25");
+	break;
+	case 16:printf("The day is TUESDAY ON 16/9/25");
+	break;
+	case 17:printf("The day is WEDNESDAY ON 17/9/25");
+	break;
+	case 18:printf("The day is THRUSDAY ON 18/9/25");
+	break;
+	case 19:printf("The day is FRIDAY ON 19/9/25");
+	break;
+	case 20:printf("The day is SATURDAY ON 20/9/25");
+	break;
+	case 21:printf("The day is SUNDAY ON 21/9/25");
+	break;
+	case 22:printf("The day is MONDAY ON 22/9/25");
+	break;
+	case 23:printf("The day is TUESDAY ON 23/9/25");
+	break;
+	case 24:printf("The day is WEDNESDAY ON 24/9/25");
+	break;
+	case 25:printf("The day is THRUSDAY ON 25/9/25");
+	break;
+	case 26:printf("The day is FRIDAY ON 26/9/25");
+	break;
+	case 27:printf("The day is SATURDAY ON 27/9/25");
+	break;
+	case 28:printf("The day is SUNDAY ON 28/9/25");
+	break;
+	case 29:printf("The day is MONDAY ON 29/9/25");
+	break;
+	case 30:printf("The day is TUESDAY ON 30/9/25");
+	break;
+}
+	
+	
+	getch();
+	return 0;
+}
